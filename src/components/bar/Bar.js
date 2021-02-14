4fb40bced2d330a45e6b55daae267419804eaf6c
Maja <maja.kasprzyk@gmail.com>
2021-02-14 14:45:42 +0100

    move files to folders

---
+++ src/components/bar/Bar.js
@@ -0,0 +1,18 @@
+import "./bar.css";
+import "../shared.css";
+
+export const Bar = () => {
+    return (
+        <>
+        <nav className="navigation">
+        <h1 className="logo">My Task Bar</h1>
+        <ul className="menu">
+          <li className="add">+</li>
+          <li className="boards">Tablice</li>
+          <li className="popUp">Powiadomienia</li>
+          <li className="account">Moje konto</li>
+        </ul>
+      </nav>
+        </>
+    );
+};
