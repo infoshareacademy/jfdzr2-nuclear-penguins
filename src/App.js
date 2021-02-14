c62a99640b17a02866778f9a6a3b9bc6f7fbd754
Aga <agasmialek2020@gmail.com>
2021-02-14 19:58:37 +0100

    signup component add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 // import Login from "./components/login/Login"
+import Signup from './components/signup/Signup';
+
 // import { SimpleNavigation } from './components/navigation/SimpleNavigation';
-import {Bar} from './components/bar/Bar';
+// import {Bar} from './components/bar/Bar';
 
 function App() {
   return (
     // <SimpleNavigation />
     // <Bar />
-    <Login />
+    // <Login />
+    <Signup />
   );
 }
 console.log('start');
