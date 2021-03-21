765847f9b69b28ef4a3dddf96f4083315e211157
Aga <agasmialek2020@gmail.com>
2021-03-22 00:28:28 +0100

    navigation.js update with footer-space div

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -3,18 +3,21 @@ import '../shared.css';
 import {NavLink} from 'react-router-dom';
 export const Navigation = () => {
   return (
-    <nav className="navigation">
-      <h1 className="logo">My Task Bar</h1>
-      <ul className="menu">
-        <li className="add">
-          <NavLink className="tableAddingBtn" to={'/bartable/tableAdder'}>
-            +
-          </NavLink>
-        </li>
-        <li className="boards">Tablice</li>
-        <li className="popUp">Powiadomienia</li>
-        <li className="account">Moje konto</li>
-      </ul>
-    </nav>
+    <>
+      <nav className="navigation">
+        <h1 className="logo">My Task Bar</h1>
+        <ul className="menu">
+          <li className="add">
+            <NavLink className="tableAddingBtn" to={'/bartable/tableAdder'}>
+              +
+            </NavLink>
+          </li>
+          <li className="boards">Tablice</li>
+          <li className="popUp">Powiadomienia</li>
+          <li className="account">Moje konto</li>
+        </ul>
+      </nav>
+      <div className="footer-space-nav"></div>
+    </>
   );
 };
